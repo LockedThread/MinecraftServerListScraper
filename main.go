2857f71d15b62d2882b445e6b26f72a71c70efc4
LockedThread <proteinshakedev@gmail.com>
2019-09-30 21:00:44 -0400

    Code commit

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"log"
+	"time"
+)
+
+const pages = 10
+
+func main() {
+	var allIps []string
+
+	for i := 1; i <= pages; i++ {
+		go func(index int) {
+			ips := scrape(index)
+			allIps = append(allIps, ips...)
+		}(i)
+	}
+
+	time.Sleep(time.Second)
+
+	for index := range allIps {
+		fmt.Println(allIps[index])
+	}
+}
+
+func scrape(page int) []string {
+	var serverAddresses []string
+
+	pageUrl := fmt.Sprintf("https://minecraft-server-list.com/page/%d/", page)
+
+	fmt.Println("Preparing to scrape " + pageUrl)
+
+	document, err := goquery.NewDocument(pageUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	document.Find("tr td").Each(func(index int, selection *goquery.Selection) {
+		for nodeIndex := range selection.Nodes {
+			node := selection.Nodes[nodeIndex]
+			for attrIndex := range selection.Nodes[nodeIndex].Attr {
+				attribute := node.Attr[attrIndex]
+				if attribute.Key == "id" {
+					serverAddresses = append(serverAddresses, attribute.Val)
+				}
+			}
+		}
+	})
+	return serverAddresses
+}
